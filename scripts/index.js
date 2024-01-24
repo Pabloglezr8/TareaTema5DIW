5faca418db188b75b7612ec4b7b9f6c966b8713d
Pabloglezr8 <pabligoru99@gmail.com>
2024-01-24 12:55:44 +0100

    menu hamburguesa

---
+++ scripts/index.js
@@ -0,0 +1,16 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const mobileMenuButton = document.getElementById('mobile-menu');
+    const hamburger = document.querySelector('.hamburger');
+
+    mobileMenuButton.addEventListener('click', function () {
+        hamburger.classList.toggle('show');
+        mobileMenuButton.classList.toggle('active');
+    });
+
+    window.addEventListener('resize', function () {
+        if (window.innerWidth > 600) {
+            hamburger.classList.remove('show');
+            mobileMenuButton.classList.remove('active');
+        }
+    });
+});
